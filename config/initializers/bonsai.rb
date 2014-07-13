71b5d189fc66b4faf07ca3da66c4811fe2d325d0
fgc29 <fgc0077@gmail.com>
2014-07-13 01:56:45 -0400

    troubleshooting app

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,5 @@
+
+
 if ENV['BONSAI_INDEX_URL']
   Tire.configure do
     url "http://index.bonsai.io"
@@ -7,3 +9,5 @@ else
   app_name = Rails.application.class.parent_name.underscore.dasherize
   BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
 end
+
+ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
