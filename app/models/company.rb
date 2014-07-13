bca076fea734d52375d007481cac03ec6193c933
fgc29 <fgc0077@gmail.com>
2014-07-13 01:31:19 -0400

    added bonsai functionality

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,8 @@ class Company < ActiveRecord::Base
 
   searchkick
 
+  index_name BONSAI_INDEX_NAME
+
   has_and_belongs_to_many :users
   validates :name, :url, :address, presence: true
   validates :name, :url, uniqueness: true
